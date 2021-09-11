662e82e1ecc7390979db780ee9c42b43a5e6d343
Hardik Pandey <hardikpandey1998@gmail.com>
2021-09-12 00:42:52 +0530

    Task Completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react"
+import jsPDF from "jspdf"
+import Resume from "./Resume"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const generatePdf = () => {
+  const doc = new jsPDF("p", "pt", "a4")
+  doc.html(document.querySelector("#a4-sheet"), {
+    callback: function (pdf) {
+      pdf.save("converted-resume.pdf")
+    }
+  })
 }
 
-export default App;
+const App = () => {
+  return (
+    <Fragment>
+      <div className="mb5">
+        <button className="generate-pdf-btn" onClick={() => generatePdf()}>Download</button>
+      </div>
+      <div id="a4-sheet" className="a4-sheet">
+        <Resume />
+      </div>
+    </Fragment>
+  )
+}
+
+export default App
